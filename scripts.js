9fc3f7500d120ce97ec637bf20e9bc36191c18fc
alekslukas <92292208+alekslukas@users.noreply.github.com>
2022-01-06 15:20:53 +0100

    moving project to GIT

---
+++ scripts.js
@@ -0,0 +1,66 @@
+function displaySearch(response) {
+    document.querySelector("#citySearch").innerHTML = response.data.name;
+    document.querySelector("#nowT").innerHTML = `${Math.round(
+      response.data.main.temp
+    )} °C`;
+    document.querySelector("#feels").innerHTML = `Feels like ${Math.round(
+      response.data.main.feels_like
+    )}°C`;
+    document.querySelector(
+      "#wind"
+    ).innerHTML = `Wind: ${response.data.wind.speed} m/s`;
+    document.querySelector(
+      "#pressure"
+    ).innerHTML = `Pressure: ${response.data.main.pressure}hPa`;
+    document.querySelector(
+      "#humidity"
+    ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+    document.querySelector("#descriptionW").innerHTML =
+      response.data.weather[0].description;
+    document.querySelector("#icon").innerHTML = response.data.weather[0].icon;
+  }
+  
+  function search(city) {
+    let units = "metric";
+    let key = "a3c7d7e43998da127d7076ec879183cf";
+  
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
+    axios.get(url).then(displaySearch);
+  }
+  
+  function showCity(event) {
+    event.preventDefault();
+    let city = document.querySelector("#city-input").value;
+    search(city);
+  }
+  
+  let cityName = document.querySelector("#search-form");
+  cityName.addEventListener("submit", showCity);
+  
+  search("Gdansk");
+  
+  let now = new Date();
+  let date = now.getDate();
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = weekDays[now.getDay()];
+  let timeH = now.getHours();
+  if (timeH < 10) {
+    timeH = `0${timeH}`;
+  }
+  let timeM = now.getMinutes();
+  if (timeM < 10) {
+    timeM = `0${timeM}`;
+  }
+  let nowDay = document.querySelector("#date");
+  nowDay.innerHTML = `${day},`;
+  let nowTime = document.querySelector("#time");
+  nowTime.innerHTML = `${timeH}:${timeM}`;
+  
