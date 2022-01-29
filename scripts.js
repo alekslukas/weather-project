d388577df39105ae26c1499d93b355a6ede6d06a
alekslukas <92292208+alekslukas@users.noreply.github.com>
2022-01-29 16:11:02 +0100

    forecast: creating string and looping it, building api call

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+function getForecast(coordinates){
+  console.log(coordinates);
+  let key = "a3c7d7e43998da127d7076ec879183cf";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`; 
+  console.log(url);
+}
+
+
 function displaySearch(response) {
+  
+    getForecast(response.data.coord);
     celciusTemp = response.data.main.temp;
     document.querySelector("#citySearch").innerHTML = response.data.name;
     document.querySelector("#nowT").innerHTML = Math.round(
@@ -98,7 +108,10 @@ function showForecast() {
   let forecast = document.querySelector("#forecast");
 
   let forecastHTLM = `<div class= "row">`;
-  forecastHTLM = forecastHTLM +
+
+let days =[ "Monday", "Tuesday", "Wednesday"]; 
+days.forEach(function(day){
+   forecastHTLM = forecastHTLM +
    `
    <div class="col-2">
    <div class="forecast-date">${day}</div>
@@ -111,19 +124,8 @@ function showForecast() {
    </div>
  </div>
   `
-  forecastHTLM = forecastHTLM +
-   `
-   <div class="col-2">
-   <div class="forecast-date">${day}</div>
-   <img
-     src="http://openweathermap.org/img/wn/50d@2x.png"
-     width="42"
-   />
-   <div class="forecast-temperatures">
-     <span class="temperature-next"> 18° </span>
-   </div>
- </div>
-  `;
+})
+
   forecastHTLM = forecastHTLM + `</div>`;
   forecast.innerHTML = forecastHTLM; 
 }
